c161945081e8c142042eb1be82fd1f2af8c3ce4d
ayatsujisrd <shiyu.wang@wengegroup.com>
2022-11-16 21:32:18 +0800

    feat: add reply controller

--- src/controllers/addReply.ts
+++ src/controllers/addReply.ts
@@ -1,14 +1,28 @@
+import { ObjectID } from "bson";
 import { Request, Response } from "express";
+import { ClientRequest } from "http";
 import { dbName } from "../configs/db.config";
 import DBService from "../service/db.service";
-import { getCurrent } from "../utils";
+import { dataWrapper, getCurrent } from "../utils";
 
 async function addReply(req: Request, res: Response) {
-  const {reply, id, name} = req.body
+  const { reply, id, replier } = req.body
   const createTime = getCurrent()
 
   const [messages, client] = DBService(dbName, 'messages')
-
+  try {
+    const id = new ObjectID()
+    const result = await messages.updateOne({ _id: new ObjectID(id) }, { $push: { replies: { _id: id, content: reply, name: replier, createTime } } })
+    if (result) {
+      res.json(dataWrapper({id: id.toString(), createTime}))
+    } else {
+      res.json(dataWrapper(undefined))
+    }
+  } catch (e) {
+    res.json(dataWrapper(undefined, 500, 'error'))
+  }finally{
+    client.close()
+  }
 }
 
 export default addReply
