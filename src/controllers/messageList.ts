5142abf1dc43e94103819def6fc02ab023c270dc
shiu <shiyuw0@163.com>
2022-11-20 16:59:09 +0800

    fix: get category

--- src/controllers/messageList.ts
+++ src/controllers/messageList.ts
@@ -4,18 +4,20 @@ import DBService from "../service/db.service";
 import { dataWrapper } from "../utils";
 
 async function messageList(req: Request, res: Response) {
-  const {pageNo, pageSize} = req.body
+  const { pageNo, pageSize } = req.query
+  const { category } = req.params
   const [messages, client] = DBService(dbName, 'messages')
 
-  const skip = pageSize * (pageNo - 1)
-  const limit = pageSize
+  const skip = Number(pageSize) * (Number(pageNo) - 1)
+  const limit = Number(pageSize)
   try {
-    const result = messages.find().sort({createTime: -1}).skip(skip).limit(limit)
-    const rows = await (await result.toArray()).map((r) => ({...r,id: r._id.toString()}))
+    const result = messages.find({ category }).sort({ createTime: -1 }).skip(skip).limit(limit)
+    const rows = await (await result.toArray()).map((r) => ({ ...r, id: r._id.toString() }))
     res.json(dataWrapper(rows))
-  }catch(e) {
+  } catch (e) {
+    console.log(e)
     res.json(dataWrapper(undefined, 500, 'error'))
-  }finally {
+  } finally {
     client.close()
   }
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ const router = Router()
 
 router.post('/signin', controllers.signin)
 router.put('/signup', controllers.signup)
-router.get('/messages', controllers.messageList)
+router.get('/messages/:category', controllers.messageList)
 router.put('/addMessage', controllers.addMessage)
 router.put('/addReply', controllers.addReply)
 router.patch('/reset', controllers.reset)
