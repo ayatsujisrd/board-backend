9a240e89b13e38a5a0ea59a4f0e28c3c19d57514
shiu <shiyuw0@163.com>
2022-11-20 20:19:07 +0800

    fix: client not close

--- src/controllers/messageList.ts
+++ src/controllers/messageList.ts
@@ -4,14 +4,18 @@ import DBService from "../service/db.service";
 import { dataWrapper } from "../utils";
 
 async function messageList(req: Request, res: Response) {
-  const { pageNo, pageSize } = req.query
+  const { pageNo, pageSize, search } = req.query
   const { category } = req.params
   const [messages, client] = DBService(dbName, 'messages')
 
   const skip = Number(pageSize) * (Number(pageNo) - 1)
   const limit = Number(pageSize)
+  const query = { category }
+  if (search) {
+    Object.assign(query, { content: { $regex: new RegExp(search as string, 'i') } })
+  }
   try {
-    const result = messages.find({ category }).sort({ createTime: -1 }).skip(skip).limit(limit)
+    const result = messages.find(query).sort({ createTime: -1 }).skip(skip).limit(limit)
     const rows = await (await result.toArray()).map((r) => ({ ...r, id: r._id.toString() }))
     res.json(dataWrapper(rows))
   } catch (e) {
--- src/controllers/signin.ts
+++ src/controllers/signin.ts
@@ -1,27 +1,28 @@
+import { createCipheriv } from "crypto"
 import { Request, Response } from "express"
+import { dbName } from "../configs/db.config"
 import DBService from "../service/db.service"
 import { dataWrapper } from '../utils'
 
-const [collection, client] = DBService('board', 'users')
+const [collection, client] = DBService(dbName, 'users')
 
 async function signin(req: Request, res: Response) {
-    // res.send(req.body)
+  try {
+    const { name, password } = req.body
+    const result = await collection.findOne({
+      name,
+      password
+    })
 
-    try {
-        const { name, password } = req.body
-        const result = await collection.findOne({
-          name,
-          password
-        })
-
-        if (!result) {
-            res.json(dataWrapper({ code: 0, msg: 'user name or password wrong' }))
-        } else {
-            res.json(dataWrapper({ code: 1, msg: 'success' }))
-        }
-    } finally {
-        client.close()
+    if (!result) {
+      res.json(dataWrapper({ code: 0, msg: 'user name or password wrong' }))
+    } else {
+      res.json(dataWrapper({ code: 1, msg: 'success' }))
     }
+  } catch (e) {
+    console.error(e)
+    res.json(dataWrapper(undefined, 500, 'error'))
+  }
 
 
 }
